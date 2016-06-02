c85e513af180e23ad2f420575ba826e472e2c32d
Aaron Dhiman <aaron.dhiman@gmail.com>
2016-06-02 10:16:58 -0400

    work on SQLite and pandas

--- Lesson3-Exercises/SQLite.py
+++ Lesson3-Exercises/SQLite.py
@@ -1,4 +1,5 @@
 import sqlite3 as lite
+import pandas as pd
 
 conn = lite.connect('getting_started.db')
 
@@ -15,6 +16,35 @@ with conn:
     cur = conn.cursor()
     cur.execute("INSERT INTO cities VALUES('Washington', 'DC')")
     cur.execute("INSERT INTO cities VALUES('Houston', 'TX')")
-    cur.execute("INSERT INTO weather VALUES('Washington', 2013, 'July', 'January')")
-    cur.execute("INSERT INTO weather VALUES('Houston', 2013, 'July', 'January')")
+    cur.execute("INSERT INTO weather VALUES('Washington', 2013, 'July', 'January', 45)")
+    cur.execute("INSERT INTO weather VALUES('Houston', 2013, 'July', 'January', 54)")
 
+cities = (('Las Vegas', 'NV'),
+          ('Atlanta', 'GA'))
+
+weather = (('Las Vegas', 2013, 'July', 'December', 59),
+           ('Atlanta', 2013, 'July', 'January', 45))
+
+conn = lite.connect('getting_started.db')
+
+# insert rows by passing tuples
+with conn:
+    cur = conn.cursor()
+    cur.executemany("INSERT INTO cities VALUES(?,?)", cities)
+    cur.executemany("INSERT INTO weather VALUES(?,?,?,?,?)", weather)
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM cities")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM cities")
+    rows = cur.fetchall()
+    cols = [desc[0] for desc in cur.description]
+    df = pd.DataFrame(rows, columns=cols)
+
+    print(df.head())
